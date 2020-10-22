964205aa538b9bbbf82d26fdb71b781d82945a18
derdesz <dora.mailbox@gmail.com>
2020-10-22 17:07:52 +0200

    added Navbar component with links

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+
+
+    return (
+      <div>
+        <Link to='/pokemons'>Pokemons</Link> | <Link to='/types'>Types</Link>
+      </div>  
+    );
+
+};
+
+export default Navbar;
+
+
