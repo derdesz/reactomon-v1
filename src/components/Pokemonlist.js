e6953c953a6efd2e17a56741636908482b479166
derdesz <dora.mailbox@gmail.com>
2020-10-24 18:18:24 +0200

    refactor: added useEffect()

--- src/components/Pokemonlist.js
+++ src/components/Pokemonlist.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Pokemonlist = (props) => {
   const [pokemons, setPokemons] = useState([]);
 
-  const componentDidMount = 
-    axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
-      setPokemons(response.data.results );
-    });
-  
+    useEffect(() => {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon")
+        .then((response) => {
+          setPokemons(response.data.results );
+        });
+    }, []);
 
   function Capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
