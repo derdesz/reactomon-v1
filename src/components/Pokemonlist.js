501b3809f06633fd7e799dd6a2328853423d35fa
derdesz <dora.mailbox@gmail.com>
2020-10-24 16:54:53 +0200

    added onClick function to Pokemon names

--- src/components/Pokemonlist.js
+++ src/components/Pokemonlist.js
@@ -1,13 +1,9 @@
-
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 class Pokemonlist extends Component {
-  state = {
-    pokemons: [],
-  };
-
-
+  state = { pokemons: [] };
 
   componentDidMount() {
     axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
@@ -15,15 +11,23 @@ class Pokemonlist extends Component {
     });
   }
 
-  Capitalize(str){
+  Capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
-    }
-
+  }
 
   render() {
-    return this.state.pokemons.map((pokemon) => <div key={pokemon.name}>{this.Capitalize(pokemon.name)}</div>);
+    return this.state.pokemons.map((pokemon) => (
+      <div key={pokemon.name}>
+        <Link
+          to={"pokemons/" + pokemon.url.substring(34)}
+          onClick={() => this.props.handleOnClick(pokemon.url)}
+        >
+          {this.Capitalize(pokemon.name)}
+        </Link>
+        <br />
+      </div>
+    ));
   }
 }
 
 export default Pokemonlist;
-
