fad777da80ae645c1a52020615d88e347d2efb37
derdesz <dora.mailbox@gmail.com>
2020-10-24 16:51:57 +0200

    added Pokemondetail component

---
+++ src/components/Pokemondetail.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Pokemondetail extends Component {
+  state = {
+    name: "",
+    id: null,
+    height: null,
+  };
+
+  componentDidMount() {
+    console.log("Inside pokedetail" + this.props.url);
+    axios.get(this.props.url).then((response) => {
+      this.setState({
+        name: response.data.name,
+        id: response.data.id,
+        height: response.data.height,
+      });
+    });
+  }
+
+  Capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Name: {this.Capitalize(this.state.name)}</div>
+        <div>Id: {this.state.id}</div>
+        <div>Height: {this.state.height}</div>
+      </div>
+    );
+  }
+}
+
+export default Pokemondetail;
