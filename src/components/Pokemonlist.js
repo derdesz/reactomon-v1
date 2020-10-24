729292c8dce667b8987c9f94aef6d5a2ed18bbca
derdesz <dora.mailbox@gmail.com>
2020-10-24 17:34:23 +0200

    refactored Pokemonlist component with hooks

--- src/components/Pokemonlist.js
+++ src/components/Pokemonlist.js
@@ -1,33 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-class Pokemonlist extends Component {
-  state = { pokemons: [] };
+const Pokemonlist = (props) => {
+  const [pokemons, setPokemons] = useState([]);
 
-  componentDidMount() {
+  const componentDidMount = 
     axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
-      this.setState({ pokemons: response.data.results });
+      setPokemons(response.data.results );
     });
-  }
+  
 
-  Capitalize(str) {
+  function Capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-  render() {
-    return this.state.pokemons.map((pokemon) => (
-      <div key={pokemon.name}>
+  
+  let content = pokemons.map((pokemon) => (
+    <div key={pokemon.name}>
         <Link
           to={"pokemons/" + pokemon.url.substring(34)}
-          onClick={() => this.props.handleOnClick(pokemon.url)}
+          onClick={() => props.handleOnClick(pokemon.url)}
         >
-          {this.Capitalize(pokemon.name)}
+          {Capitalize(pokemon.name)}
         </Link>
         <br />
       </div>
-    ));
-  }
+  ))  
+  
+  
+  
+  return content;
+  
 }
 
 export default Pokemonlist;
