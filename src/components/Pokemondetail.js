794aeae1f0aa1cd0d2d9ffbbf7b918f868acfb9a
derdesz <dora.mailbox@gmail.com>
2020-10-24 18:13:42 +0200

    refactored Pokemondetail with hoocks

--- src/components/Pokemondetail.js
+++ src/components/Pokemondetail.js
@@ -1,36 +1,45 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-class Pokemondetail extends Component {
-  state = {
-    name: "",
-    id: null,
-    height: null,
-  };
+const Pokemondetail = (props) => {
+  const [name, setName] = useState("");
+  const [id, setId] = useState(null);
+  const [height, setHeight] = useState(null);
 
-  componentDidMount() {
-    axios.get(this.props.url).then((response) => {
-      this.setState({
-        name: response.data.name,
-        id: response.data.id,
-        height: response.data.height,
+  // function componentDidMount() {
+  //   axios.get(this.props.url).then((response) => {
+  //     setName(response.data.name);
+  //     setId(response.data.id);
+  //     setHeight(response.data.height);
+  //     // this.setState({
+  //     //   name: response.data.name,
+  //     //   id: response.data.id,
+  //     //   height: response.data.height,
+  //     // });
+  //   });
+  // }
+
+  useEffect(() => {
+    axios
+      .get(props.url)
+      .then((response) => {
+        setName(response.data.name);
+        setId(response.data.id);
+        setHeight(response.data.height);
       });
-    });
-  }
+  }, []);
 
-  Capitalize(str) {
+  function Capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-  render() {
-    return (
-      <div>
-        <div>Name: {this.Capitalize(this.state.name)}</div>
-        <div>Id: {this.state.id}</div>
-        <div>Height: {this.state.height}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <div>Name: {Capitalize(name)}</div>
+      <div>Id: {id}</div>
+      <div>Height: {height}</div>
+    </div>
+  );
+};
 
 export default Pokemondetail;
