a20e8e6885ec9f68a1422a88def01a03385b2487
derdesz <dora.mailbox@gmail.com>
2020-10-25 13:37:00 +0100

    handled case when pokemon is already being caught, also showing alert box

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { transitions, positions, Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
+import AlertTemplate from 'react-alert-template-oldschool-dark';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const options = {
   // you can also just use 'bottom center'
-  position: positions.BOTTOM_CENTER,
+  position: positions.TOP_CENTER,
   timeout: 5000,
   offset: '30px',
   // you can also just use 'scale'
