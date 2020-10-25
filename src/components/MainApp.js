32e0f0f751b1c6fafc252d6a650263eebadb958b
derdesz <dora.mailbox@gmail.com>
2020-10-25 12:52:01 +0100

    added list of catched pokemons & new state for catched pokemons

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import "../App.css";
 import styled from 'styled-components';
 import Navbar from "./Navbar";
-import { useHistory, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Pokemonlist from "./Pokemonlist";
 import Typelist from "./Typelist";
 import Pokemondetail from "./Pokemondetail";
+import CatchedPokemons from "./CatchedPokemons";
 
 const Containers = styled.div`
   width: 100%;                 
@@ -16,10 +17,13 @@ const Containers = styled.div`
   margin-top: 30px;
   margin-bottom: 30px;
 `;
+const catchedPokemonsList = [];
 
 const MainApp = () =>  {
     const [currentPath, setCurrentPath] = useState("");
     const [currentUrl, setCurrentUrl] = useState("");
+    const [catchedPokemons, setCatchedPokemons] = useState([]);
+
 
   function handleOnClick(url) {
       if ( url === "") {
@@ -32,6 +36,12 @@ const MainApp = () =>  {
       }
     
   };
+
+  function clickOnCatch (pokemonName) {
+    catchedPokemonsList.push(pokemonName);
+    setCatchedPokemons(catchedPokemonsList);
+    console.log(pokemonName);
+  }
 
 
 
@@ -49,11 +59,14 @@ const MainApp = () =>  {
                 <Route
                   path="/pokemons"
                   render={(props) => (
-                    <Pokemonlist {...props} handleOnClick={handleOnClick} />
+                    <Pokemonlist {...props} handleOnClick={handleOnClick} clickOnCatch={clickOnCatch}/>
                   )}
                 />
                 <Containers>
                   <Route path="/types" component={Typelist} />
+                </Containers>
+                <Containers>
+                  <Route path="/catched" render={(props) => <CatchedPokemons {...props} catchedPokemonsList={catchedPokemons}/>}/>
                 </Containers>
             </Router>
           </div>
@@ -64,7 +77,7 @@ const MainApp = () =>  {
             <Router>
                 <Navbar handleOnClick={handleOnClick}/>
             </Router>
-            <Pokemondetail url={currentUrl} />
+            <Pokemondetail url={currentUrl} clickOnCatch={clickOnCatch}/>
         </div>
       </div>
       );
