d4888a4258183301fbcda8add3433c112c23ef68
derdesz <dora.mailbox@gmail.com>
2020-10-25 12:49:36 +0100

    removed unnecessary styled component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import MainApp from './components/MainApp';
-import styled from 'styled-components';
-
-const HTML = styled.html`
-  background: url(pokemon_background.png) no-repeat center center fixed; 
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-`
-
 
 class App extends Component {
 
