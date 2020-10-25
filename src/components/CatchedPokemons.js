6dada71e2b1fd2a14c73c5339fbd136cd0b26f60
derdesz <dora.mailbox@gmail.com>
2020-10-25 14:21:19 +0100

    add remove button

--- src/components/CatchedPokemons.js
+++ src/components/CatchedPokemons.js
@@ -15,15 +15,33 @@ const StyledType = styled.div`
   background-color: rgba(81 51 68 / 65%);
 `;
 
+const Button = styled.button`
+  color: white;
+  font-size: 7px;
+  font-weight: bold;
+  border: none;
+  border-radius: 1rem;
+  background-color: rgba(200 0 126 / 80%);
+  margin-left: 220px;
+
+`;
+
 
 
 const CatchedPokemons = (props) => {
     
   if (props.catchedPokemonsList.length > 0) {
     return props.catchedPokemonsList.map((name) => (
-      <StyledType key={name}>{name.charAt(0).toUpperCase() + name.slice(1)}</StyledType>
+      <StyledType key={name}>
+        {name.charAt(0).toUpperCase() + name.slice(1)}
+        <Button onClick={() => props.clickOnDelete(name)}>X</Button>
+      </StyledType>
     ));
-  } else return <StyledType>No catched pokemons...</StyledType>
+  } else return (
+  <StyledType>
+    No catched pokemons...
+  </StyledType>
+  );
   
 };
 
